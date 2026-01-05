48288e6e4c8b340f7ab79706798a85436bbecf78
AaltonenJari <jaritapani.aaltonen@gmail.com>
2026-01-05 13:49:25 +0200

    5.5 blogilistan frontend, step5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { useState, useEffect } from 'react'
-import Blog, { BlogAdditionForm } from './components/Blog'
+import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/notification'
+import Togglable from './components/Togglable'
+import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')  
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [newTitle, setTitle] = useState('')
-  const [newAuthor, setAuthor] = useState('')
-  const [newUrl, setUrl] = useState('')
   const [notificationMessage, setNotificationMessage] = useState(null)
   const [notificationStatus, setNotificationStatus] = useState(null)
 
@@ -62,13 +62,25 @@ const App = () => {
     setPassword('')
   }
 
-  const addBlog = (event) => {
-    event.preventDefault()
-    const blogObject = {
-      title: newTitle,
-      author: newAuthor,
-      url: newUrl
-    }
+  const loginForm = () => {
+
+    return (
+      <div>
+        <Togglable buttonLabel='login'>
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+            notificationMessage={notificationMessage}
+          />
+        </Togglable>
+      </div>
+    )
+  }
+
+  const addBlog = (blogObject) => {
     blogService.create(blogObject).then(returnedBlog => {
       setBlogs(blogs.concat(returnedBlog))
 
@@ -79,10 +91,6 @@ const App = () => {
         setNotificationMessage(null)
         setNotificationStatus(null)
       }, 5000)
-
-      setTitle('')
-      setAuthor('')
-      setUrl('')
     })
     .catch(error => {
       const notificationMessage = `error creating blog: ${error.response.data.error}`
@@ -99,46 +107,16 @@ const App = () => {
   if (user === null) {
     return (
       <div>
-        <h2>Log in to application</h2>
-        <Notification status="error" message={notificationMessage} />
-        <form onSubmit={handleLogin}>
-          <div>
-            <label>
-              username
-              <input
-                type="text"
-                value={username}
-                onChange={({ target }) => setUsername(target.value)}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              password
-              <input
-                type="password"
-                value={password}
-                onChange={({ target }) => setPassword(target.value)}
-              />
-            </label>
-          </div>
-          <button type="submit">login</button>
-        </form>
+        {loginForm()}
       </div>
     )
   }
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value)
-  }
-
-  const handleAuthorChange = (event) => {
-    setAuthor(event.target.value)
-  }
-
-  const handleUrlChange = (event) => {
-    setUrl(event.target.value)
-  }
+ const BlogFormTogglable = () => (
+  <Togglable buttonLabel="new blog">
+    <BlogForm createBlog={addBlog} />
+  </Togglable>
+)
 
   return (
     <div>
@@ -152,17 +130,7 @@ const App = () => {
         </p>
       </form>
 
-      <h2>Create new</h2>
-
-      <BlogAdditionForm  
-        addBlog={addBlog}
-        newTitle={newTitle}
-        handleTitleChange={handleTitleChange}
-        newAuthor={newAuthor}
-        handleAuthorChange={handleAuthorChange}
-        newUrl={newUrl}
-        handleUrlChange={handleUrlChange}
-      />
+      <BlogFormTogglable />
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
