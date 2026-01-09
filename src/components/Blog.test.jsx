7f025ab4bea4b4c7b506ce913b6c7ee155504008
AaltonenJari <jaritapani.aaltonen@gmail.com>
2026-01-09 15:06:29 +0200

    5.14: blogilistan testit, step2

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -13,4 +14,27 @@ test('renders content', () => {
 
   const element = screen.getByText(/Component testing is done with react-testing-library/)
   expect(element).toBeInTheDocument()
+})
+
+test('shows url, likes and user after clicking view button', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Test Author',
+    url: 'http://example.com',
+    likes: 5,
+    user: {
+      id: '123',
+      name: 'Test User'
+    }
+  }
+
+  const { container } = render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  await user.click(screen.getByText('view'))
+
+  expect(container).toHaveTextContent('http://example.com')
+  expect(container).toHaveTextContent('likes')
+  expect(container).toHaveTextContent('5')
+  expect(container).toHaveTextContent('Test User')
 })
