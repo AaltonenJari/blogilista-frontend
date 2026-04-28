7467c3c2231d1f38addbd4e587b54bc972b7d11b
AaltonenJari <jaritapani.aaltonen@gmail.com>
2026-04-28 16:02:44 +0300

    Yksittäisen blogin tiedot näyttävä näkymä

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ import {
   BrowserRouter as Router,
   Routes, Route, Link, Navigate
 } from 'react-router-dom'
+import Blog from './components/Blog'
 
 const App = () => {
   const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([])
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [notificationStatus, setNotificationStatus] = useState(null)
 
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
@@ -29,15 +33,90 @@ const App = () => {
     padding: 5
   }
 
-  /*
-  if (user === null) {
-    return (
-      <div>
-        <Login setUser={setUser} />
-      </div>
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
     )
+  }, [])
+
+  const increaseLikesOf = (id) => {
+    const blog = blogs.find(b => b.id === id)
+    const updatedBlog = { ...blog, likes: blog.likes + 1 }
+
+    blogService.update(id, updatedBlog)
+      .then(returnedBlog => {
+        setBlogs(blogs.map(n => n.id !== id ? n : returnedBlog))
+
+        const notificationMessage = `liked blog ${returnedBlog.title} by ${returnedBlog.author}`
+        setNotificationMessage(notificationMessage)
+        setNotificationStatus('ok')
+        setTimeout(() => {
+          setNotificationMessage(null)
+          setNotificationStatus(null)
+        }, 5000)
+      })
+      .catch(error => {
+        const notificationMessage = `error updating likes: ${error.response && error.response.data && error.response.data.error ? error.response.data.error : error.message}`
+        setNotificationMessage(notificationMessage)
+        setNotificationStatus('error')
+        setTimeout(() => {
+          setNotificationMessage(null)
+          setNotificationStatus(null)
+        }, 5000)
+      })
+  }
+
+  /*
+  const addBlog = (blogObject) => {
+    noteFormRef.current.toggleVisibility()
+
+    blogService.create(blogObject).then(returnedBlog => {
+      setBlogs(blogs.concat(returnedBlog))
+
+      const notificationMessage = `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`
+      setNotificationMessage(notificationMessage)
+      setNotificationStatus('ok')
+      setTimeout(() => {
+        setNotificationMessage(null)
+        setNotificationStatus(null)
+      }, 5000)
+    })
+      .catch(error => {
+        const notificationMessage = `error creating blog: ${error.response.data.error}`
+        setNotificationMessage(notificationMessage)
+        setNotificationStatus('error')
+        setTimeout(() => {
+          setNotificationMessage(null)
+          setNotificationStatus(null)
+        }, 5000)
+      })
   }
 */
+
+
+  const deleteBlog = (id) => {
+    blogService.remove(id)
+      .then(() => {
+        setBlogs(blogs.filter(b => b.id !== id))
+
+        const notificationMessage = 'blog deleted'
+        setNotificationMessage(notificationMessage)
+        setNotificationStatus('ok')
+        setTimeout(() => {
+          setNotificationMessage(null)
+          setNotificationStatus(null)
+        }, 5000)
+      })
+      .catch(error => {
+        const notificationMessage = `error deleting blog: ${error.response && error.response.data && error.response.data.error ? error.response.data.error : error.message}`
+        setNotificationMessage(notificationMessage)
+        setNotificationStatus('error')
+        setTimeout(() => {
+          setNotificationMessage(null)
+          setNotificationStatus(null)
+        }, 5000)
+      })
+  }
 
   return (
     <Router>
@@ -55,10 +134,21 @@ const App = () => {
         )}
       </div>
 
-      <Notification />
+      <Notification status={notificationStatus} message={notificationMessage} />
 
       <Routes>
-        <Route path="/" element={<BlogList user={user} />} />
+        <Route
+          path="/blogs/:id"
+          element={
+            <Blog
+              blogs={blogs}
+              userid={user?.id}
+              increaseLikes={increaseLikesOf}
+              deleteBlog={deleteBlog}
+            />
+          }
+        />
+        <Route path="/" element={<BlogList blogList={blogs} />} />
 
         <Route
           path="/login"
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,112 +1,17 @@
-import { useState, useEffect } from 'react'
-import blogService from '../services/blogs'
-import Notification from './notification'
-import Blog from './Blog'
+import { Link } from 'react-router-dom'
 
-const BlogList = (user) => {
-  const [blogs, setBlogs] = useState([])
-  const [notificationMessage, setNotificationMessage] = useState(null)
-  const [notificationStatus, setNotificationStatus] = useState(null)
-
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )
-  }, [])
-
-  const increaseLikesOf = (id) => {
-    const blog = blogs.find(b => b.id === id)
-    const updatedBlog = { ...blog, likes: blog.likes + 1 }
-
-    blogService.update(id, updatedBlog)
-      .then(returnedBlog => {
-        setBlogs(blogs.map(n => n.id !== id ? n : returnedBlog))
-
-        const notificationMessage = `liked blog ${returnedBlog.title} by ${returnedBlog.author}`
-        setNotificationMessage(notificationMessage)
-        setNotificationStatus('ok')
-        setTimeout(() => {
-          setNotificationMessage(null)
-          setNotificationStatus(null)
-        }, 5000)
-      })
-      .catch(error => {
-        const notificationMessage = `error updating likes: ${error.response && error.response.data && error.response.data.error ? error.response.data.error : error.message}`
-        setNotificationMessage(notificationMessage)
-        setNotificationStatus('error')
-        setTimeout(() => {
-          setNotificationMessage(null)
-          setNotificationStatus(null)
-        }, 5000)
-      })
-  }
-
-  /*
-  const addBlog = (blogObject) => {
-    noteFormRef.current.toggleVisibility()
-
-    blogService.create(blogObject).then(returnedBlog => {
-      setBlogs(blogs.concat(returnedBlog))
-
-      const notificationMessage = `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`
-      setNotificationMessage(notificationMessage)
-      setNotificationStatus('ok')
-      setTimeout(() => {
-        setNotificationMessage(null)
-        setNotificationStatus(null)
-      }, 5000)
-    })
-      .catch(error => {
-        const notificationMessage = `error creating blog: ${error.response.data.error}`
-        setNotificationMessage(notificationMessage)
-        setNotificationStatus('error')
-        setTimeout(() => {
-          setNotificationMessage(null)
-          setNotificationStatus(null)
-        }, 5000)
-      })
-  }
-*/
-
-  const deleteBlog = (id) => {
-    blogService.remove(id)
-      .then(() => {
-        setBlogs(blogs.filter(b => b.id !== id))
-
-        const notificationMessage = 'blog deleted'
-        setNotificationMessage(notificationMessage)
-        setNotificationStatus('ok')
-        setTimeout(() => {
-          setNotificationMessage(null)
-          setNotificationStatus(null)
-        }, 5000)
-      })
-      .catch(error => {
-        const notificationMessage = `error deleting blog: ${error.response && error.response.data && error.response.data.error ? error.response.data.error : error.message}`
-        setNotificationMessage(notificationMessage)
-        setNotificationStatus('error')
-        setTimeout(() => {
-          setNotificationMessage(null)
-          setNotificationStatus(null)
-        }, 5000)
-      })
-  }
-
-  const userId = user.user ? user.user.id : 0
+const BlogList = ({ blogList = [] }) => {
   return (
     <div>
       <h2>blogs</h2>
-      <Notification status={notificationStatus} message={notificationMessage} />
 
-      {[...blogs].sort((a, b) => b.likes - a.likes).map(blog =>
-        <Blog
-          key={blog.id}
-          blog={blog}
-          userid={userId}
-          increaseLikes={() => increaseLikesOf(blog.id)}
-          deleteBlog={deleteBlog}
-        />
-      )}
+      <ul>
+        {blogList.map(blog => (
+          <li key={blog.id}>
+            <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
