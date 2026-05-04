f3b3d6aca564d4b1ba2942de501bf430adf15ad4
AaltonenJari <jaritapani.aaltonen@gmail.com>
2026-05-04 15:10:37 +0300

    Tehtävä 5.27: routed blogs, step4

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -13,24 +13,24 @@ const Blog = ({ blog, userid, increaseLikes, deleteBlog }) => {
     }
   }
 
-  const userActionButtonVisible = { display: blog.user && blog.user.id === userid ? '' : 'none' }
-
   return (
-    <div data-testid={`blog-${id}`}>
+    <div data-testid={`blog-${blog.id}`}>
       <h1>{blog.author}: {blog.title}</h1>
       <a href={blog.url} target="_blank" rel="noopener noreferrer">{blog.url}</a>
 
       <div>
-        <span data-testid={`likes-${id}`}>likes {blog.likes}</span>
-        <span style={userActionButtonVisible}>
-          <button onClick={() => increaseLikes(id)} data-testid={`like-${id}`}>like</button>
+        <span data-testid={`likes-${blog.id}`}>likes {blog.likes}</span>
+        <span>
+          {userid && (
+            <button onClick={() => increaseLikes(blog.id)} data-testid={`like-button-${blog.id}`}>like</button>
+          )}
         </span>
       </div>
       <span>Added by {blog.user.name}</span>
-      <div style={userActionButtonVisible}>
-        <button onClick={handleDelete}>
-          remove
-        </button>
+      <div>
+        {blog.user && blog.user.id === userid && (
+          <button onClick={handleDelete} data-testid={`delete-button-${blog.id}`}>remove</button>
+        )}
       </div>
     </div>
   )
