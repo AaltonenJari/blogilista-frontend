d6b684b3ec030fe0da3bae8bc125d9a7c3a609cd
AaltonenJari <jaritapani.aaltonen@gmail.com>
2026-05-11 14:34:38 +0300

    Tehtävä 5.31: styled blogs, step3

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,4 +1,5 @@
 import { useNavigate, useParams } from 'react-router-dom'
+import { Button, Typography } from '@mui/material'
 
 const Blog = ({ blog, userid, increaseLikes, deleteBlog }) => {
   const navigate = useNavigate()
@@ -13,25 +14,60 @@ const Blog = ({ blog, userid, increaseLikes, deleteBlog }) => {
     }
   }
 
+  const padding = {
+    padding: '25px 5px 5px 5px'
+  }
+
+  const paddingText = {
+    padding: 5
+  }
+
   return (
     <div data-testid={`blog-${blog.id}`}>
-      <h1>{blog.author}: {blog.title}</h1>
-      <a href={blog.url} target="_blank" rel="noopener noreferrer">{blog.url}</a>
 
-      <div>
-        <span data-testid={`likes-${blog.id}`}>likes {blog.likes}</span>
-        <span>
-          {userid && (
-            <button onClick={() => increaseLikes(blog.id)} data-testid={`like-button-${blog.id}`}>like</button>
-          )}
-        </span>
+      <Typography variant="h5" style={padding}>
+        {blog.title}
+      </Typography>
+      <div style={paddingText}>by {blog.author}</div>
+      <div style ={paddingText}>
+        <a href={blog.url} target="_blank" rel="noopener noreferrer">
+          {blog.url}
+        </a>
       </div>
-      <span>Added by {blog.user.name}</span>
-      <div>
+
+      <div style={paddingText}>
+        <span>Added by {blog.user.name}</span>
+      </div>
+
+      <div style={paddingText}>
+        <span data-testid={`likes-${blog.id}`}>
+          {blog.likes} likes
+        </span>
+
+        {userid && (
+          <Button
+            onClick={() => increaseLikes(blog.id)}
+            data-testid={`like-button-${blog.id}`}
+            variant="outlined"
+            sx={{ ml: 1 }}
+          >
+            like
+          </Button>
+        )}
+
         {blog.user && blog.user.id === userid && (
-          <button onClick={handleDelete} data-testid={`delete-button-${blog.id}`}>remove</button>
+          <Button
+            onClick={handleDelete}
+            data-testid={`delete-button-${blog.id}`}
+            variant="outlined"
+            color="error"
+            sx={{ ml: 1 }}
+          >
+            remove
+          </Button>
         )}
       </div>
+
     </div>
   )
 }
